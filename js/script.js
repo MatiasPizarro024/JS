94cf8b1b8a0e40f80fb18c01f5719cd39d4c080b
matiaspizarro024 <pizarro.matias.epet20@gmail.com>
2022-10-11 18:04:43 -0300

    Trabajo terminado

---
+++ js/script.js
@@ -0,0 +1,27 @@
+nombre = `Matias`;
+apellido = `Pizarro`
+edad = 18;
+
+var array = [`Banana`, `Sandia`, `Naranja`, `Uva`]
+const resArregl = array.forEach(function(element, i) {
+    console.log(`${element} se encuentra en el indice ${i}`);
+});
+
+array = { nombre, apellido, edad }
+
+var n1 = 56;
+var n2 = 44;
+
+const suma = "La suma es: ";
+const resta = "La resta es: ";
+
+let num1 = 103;
+let num2 = 23;
+
+
+if (num1 > num2) {
+    rest = num1 - num2;
+}
+
+console.log(suma, ``, n1 + n2);
+console.log(resta, ``, num1 - num2);
